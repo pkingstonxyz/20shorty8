dbc8c7ab3d0f99950e9a3dc50bddecbce4ab52a8
Patrick Kingston <patrick@pkingston.xyz>
2024-07-27 00:33:47 -0500

    Basic 3d scene

--- twentyshortyeight/src/app/core.cljs
+++ twentyshortyeight/src/app/core.cljs
@@ -1,10 +1,15 @@
 (ns app.core
   (:require
     [uix.core :as uix :refer [defui $]]
-    [uix.dom]))
+    [uix.dom]
+    ["@react-three/fiber" :as r3f]))
 
 (defui app []
-  ($ :p "hello"))
+  ($ r3f/Canvas #js {:style #js {:height "750px" :width "750px"}}
+     ($ :ambientLight #js {:intensity 1.62})
+     ($ :mesh
+        ($ :boxGeometry #js {:args #js [1 1 1]})
+        ($ :meshBasicMaterial #js {:color "#ffff00"})))) 
 
 (defonce root
   (uix.dom/create-root (js/document.getElementById "root")))
